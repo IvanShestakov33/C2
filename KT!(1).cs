68a13c3c8fc4e786fda9ca817a504babd28e7d30
IvanShestakov33 <163540627+IvanShestakov33@users.noreply.github.com>
2024-03-18 21:09:53 +0500

    Add files via upload

---
+++ KT!(1).cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+{
+
+
+    Console.WriteLine("Hello, World!");
+    Console.Write("Hello ");
+    Console.Write("World ");
+    Console.Write("!!! ");
+    Console.ReadKey();  
+}
